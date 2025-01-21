62ffe226c9cd23e795b2dcbfd37550ef44578d28
Anderson Rodrigues <and.rt@hotmail.com>
2025-01-21 19:06:02 -0300

    Initial project setup with Spring Boot, dependencies, and basic configurations

---
+++ checkout-api/src/test/java/com/qikserve/checkout_api/CheckoutApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qikserve.checkout_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckoutApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
