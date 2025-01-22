df9f304fe8513427a08676da12f584810ace3ee6
Anderson Rodrigues <and.rt@hotmail.com>
2025-01-22 11:58:07 -0300

    Implemented ProductProxy to consume Wiremock mock API

--- checkout-api/src/main/java/com/qikserve/checkout_api/CheckoutApiApplication.java
+++ checkout-api/src/main/java/com/qikserve/checkout_api/CheckoutApiApplication.java
@@ -2,8 +2,10 @@ package com.qikserve.checkout_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class CheckoutApiApplication {
 
 	public static void main(String[] args) {
---
+++ checkout-api/src/main/java/com/qikserve/checkout_api/proxy/ProductProxy.java
@@ -0,0 +1,17 @@
+package com.qikserve.checkout_api.proxy;
+
+import com.qikserve.checkout_api.model.Product;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(name = "ProductClient", url = "http://localhost:8081")
+public interface ProductProxy {
+    @GetMapping("/products")
+    List<Product> getAllProducts();
+
+    @GetMapping("/products/{productId}")
+    Product getProductById(@PathVariable("productId") String productId);
+}
